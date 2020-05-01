76b2709ae4ac399ec8c695f1c2b95d9fc78410e6
Joonas Lehtimäki <joonas.lehtimaki@polarsquad.com>
2020-05-01 19:59:14 +0300

    Go FMT

--- aws.go
+++ aws.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-  "github.com/aws/aws-sdk-go/aws/credentials"
-  "github.com/aws/aws-sdk-go/aws/credentials/stscreds"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/sts"
-  "github.com/sirupsen/logrus"
-  "os"
-  "os/exec"
-  "time"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"time"
 )
 
 const awsCliExe = "aws"
 
 func awsGetKubeConfig(clusterName string, region string) *exec.Cmd {
-  return exec.Command(awsCliExe, "eks","--region",region,"update-kubeconfig","--name",clusterName)
+	return exec.Command(awsCliExe, "eks", "--region", region, "update-kubeconfig", "--name", clusterName)
 }
 
 func assumeRole(roleArn string) {
-  client := sts.New(session.New())
-  duration := time.Hour * 1
-  stsProvider := &stscreds.AssumeRoleProvider{
-    Client:          client,
-    Duration:        duration,
-    RoleARN:         roleArn,
-    RoleSessionName: "drone",
-  }
+	client := sts.New(session.New())
+	duration := time.Hour * 1
+	stsProvider := &stscreds.AssumeRoleProvider{
+		Client:          client,
+		Duration:        duration,
+		RoleARN:         roleArn,
+		RoleSessionName: "drone",
+	}
 
-  value, err := credentials.NewCredentials(stsProvider).Get()
-  if err != nil {
-    logrus.WithFields(logrus.Fields{
-      "error": err,
-    }).Fatal("Error assuming role!")
-  }
-  os.Setenv("AWS_ACCESS_KEY_ID", value.AccessKeyID)
-  os.Setenv("AWS_SECRET_ACCESS_KEY", value.SecretAccessKey)
-  os.Setenv("AWS_SESSION_TOKEN", value.SessionToken)
+	value, err := credentials.NewCredentials(stsProvider).Get()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("Error assuming role!")
+	}
+	os.Setenv("AWS_ACCESS_KEY_ID", value.AccessKeyID)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", value.SecretAccessKey)
+	os.Setenv("AWS_SESSION_TOKEN", value.SessionToken)
 }
